ee6ba56d7bec6d2a789d45b5be4d1b96d92c931a
Kondro571 <kondziot2000@gmail.com>
2025-05-08 15:10:21 +0200

    p3 fixes

--- projekt 3/main.py
+++ projekt 3/main.py
@@ -3,11 +3,11 @@ import logging
 import pygad
 import numpy
 import benchmark_functions as bf
-#from opfunu.cec_based.cec2014 import F132014
+from opfunu.cec_based.cec2014 import F132014
 import numpy as np
 
 from mutayion import gaussian_mutation,swap_mutation
-from int_crossover import single_point_crossover_pygad,two_point_crossover_pygad,uniform_crossover_pygad
+from int_crossover import single_point_crossover_pygad,two_point_crossover_pygad,uniform_crossover_pygad,make_uniform_crossover_pygad
 from float_crossover import arithmetic_crossover_real,linear_crossover_real,blend_crossover_alpha_real,blend_crossover_alpha_beta_real,average_crossover,make_arithmetic_crossover_real,make_blend_crossover_alpha_beta,make_blend_crossover_alpha_real
 #Konfiguracja algorytmu genetycznego
 def get_function(name, ndim):
@@ -15,17 +15,17 @@ def get_function(name, ndim):
     if name == "Hypersphere":
         return bf.Hypersphere(n_dimensions=ndim)
     elif name == "Shifted and Rotated HappyCat Function":
-        return bf.Hypersphere(n_dimensions=ndim)
 
-        # func = F132014(ndim=ndim)
-        # return func.evaluate
-        
-num_vars = 3
+
+        func = F132014(ndim=ndim)
+        return func.evaluate
+
+num_vars = 5
 bits_per_var = 20
 num_genes = num_vars * bits_per_var
 
 func = bf.Ackley(n_dimensions=num_genes)
-
+func = get_function("Hypersphere", num_genes)
 num_generations = 100
 sol_per_pop = 80
 num_parents_mating = 50
@@ -33,7 +33,6 @@ num_parents_mating = 50
 init_range_low = 0
 init_range_high = 2
 gene_type = int
-
 
 mutation_num_genes = 1
 parent_selection_type = "tournament"
@@ -76,23 +75,20 @@ def on_generation(ga_instance):
 
 
 def decodeInd(individual):
-    bits_per_var = 20
-    num_vars = 2
-    bounds = func.suggested_bounds()
-        
+
+    bounds = [(-100, 100)] * num_vars
+    #bounds = [(init_range_low, init_range_high)] * num_vars
+
     decoded = []
     for i in range(num_vars):
         bits = individual[i * bits_per_var:(i + 1) * bits_per_var]
         bitstring = ''.join(str(int(b)) for b in bits)
         value = int(bitstring, 2)
-        min_val = bounds[0][i]
-        max_val = bounds[1][i]
-
+        min_val, max_val = bounds[i]
         max_binary = 2 ** bits_per_var - 1
         norm_val = min_val + (value / max_binary) * (max_val - min_val)
         decoded.append(norm_val)
     return decoded
-
 
 def fitnessFunction(ga_instance, solution, solution_idx):
     ind = decodeInd(solution)
@@ -105,24 +101,24 @@ def fitness_func(ga_instance, solution, solution_idx):
     return 1./fitness
 
 #Właściwy algorytm genetyczny
-custom_crossover = make_blend_crossover_alpha_beta(alpha=0.3, beta=0.7)
+custom_crossover = make_uniform_crossover_pygad(0.7)
 ga_instance = pygad.GA(
             num_generations=num_generations,
             sol_per_pop=sol_per_pop,
             num_parents_mating=num_parents_mating,
             num_genes=num_genes,
-            gene_type=float,
+            gene_type=int,
             init_range_low=init_range_low,
             init_range_high=init_range_high,
             mutation_num_genes=mutation_num_genes,
             parent_selection_type=parent_selection_type,
-            crossover_type=average_crossover,#ga_instance = pygad.GA(..., crossover_type=single_point_crossover_pygad, ...)
+            crossover_type=custom_crossover,#ga_instance = pygad.GA(..., crossover_type=single_point_crossover_pygad, ...)
             #crossover_func=single_point_crossover_pygad,
             mutation_type=mutation_type,
             keep_elitism=1,
             K_tournament=3,
             logger=logger,
-            fitness_func=fitness_func,
+            fitness_func=fitnessFunction,
             on_generation=on_generation,
             parallel_processing=["thread", 4]
         )
