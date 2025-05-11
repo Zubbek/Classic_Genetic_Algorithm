fe64461235f5baf3b0b5779540f43f65c949394a
Piotr Świebocki <swpiotr5@interia.pl>
2025-05-11 14:14:55 +0200

    mutacje, poprawiony wykres

---
+++ projekt3_1/mutations.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+# ====== Mutacje ======
+
+
+def swap_mutation(offspring, ga_instance):
+    for i in range(offspring.shape[0]):
+        idx1, idx2 = np.random.choice(offspring.shape[1], 2, replace=False)
+        offspring[i][idx1], offspring[i][idx2] = offspring[i][idx2], offspring[i][idx1]
+    return offspring
+
+
+def boundary_mutation(offspring, ga_instance):
+    for i in range(offspring.shape[0]):
+        if np.random.rand() < 0.5:
+            offspring[i][0] = 1 - int(round(offspring[i][0]))
+        else:
+            offspring[i][-1] = 1 - int(round(offspring[i][-1]))
+    return offspring
+
+
+def one_point_mutation(offspring, ga_instance):
+    for i in range(offspring.shape[0]):
+        idx = np.random.randint(offspring.shape[1])
+        offspring[i][idx] = 1 - int(round(offspring[i][idx]))
+    return offspring
+
+
+def two_point_mutation(offspring, ga_instance):
+    for i in range(offspring.shape[0]):
+        idx1, idx2 = np.random.choice(offspring.shape[1], 2, replace=False)
+        offspring[i][idx1] = 1 - int(round(offspring[i][idx1]))
+        offspring[i][idx2] = 1 - int(round(offspring[i][idx2]))
+    return offspring
+
